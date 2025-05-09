dd64425809748f8af5b33819e6aedfca0dd2e3be
falvfer <falvfer2612@g.educaand.es>
2025-05-09 11:30:59 +0200

    V250509

--- prPract26/src/test/Test.java
+++ prPract26/src/test/Test.java
@@ -3,22 +3,34 @@ package test;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JFrame;
+
+import controlador.Controlador;
 import modelo.BilleteTren;
 import modelo.BilleteTrenFamiliar;
 import modelo.EnumDescuentos;
 import modelo.Punto;
+import vista.Vista;
 
 public class Test {
 
 	public static void main(String[] args) {
-		List<BilleteTren> billetes = new ArrayList<>();
-		billetes.add(new BilleteTren(new Punto(), new Punto(30, 13), EnumDescuentos.MENOR_7));
-		billetes.add(new BilleteTren(new Punto(), new Punto(20, 10), EnumDescuentos.MENOR_18));
-		billetes.add(new BilleteTrenFamiliar(new Punto(100,100), new Punto(200,200), EnumDescuentos.UNIVERSITARIO, "Juanito"));
-		billetes.add(new BilleteTren(new Punto(), new Punto(-23, 5), EnumDescuentos.UNIVERSITARIO));
 		
-		for (BilleteTren b: billetes) {
-			System.out.println(b);
-		}
+		// Crear un objeto de la clase Vista
+				Vista miVista = new Vista();
+
+				// Crear un objeto de la Clase Controlador
+			//	Controlador ctr = new Controlador(miVista);
+
+				// Llamar al m�todo que da control a los componentes
+			//	miVista.control(ctr);
+
+				// Crearemos un JFrame
+				JFrame ventana = new JFrame("Venta de billetes de tren");
+				ventana.setContentPane(miVista);
+				ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				ventana.pack();
+				ventana.setVisible(true);
+		
 	}
 }
--- prPract26/src/vista/Vista.java
+++ prPract26/src/vista/Vista.java
@@ -1,11 +1,121 @@
 package vista;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+
+import controlador.Controlador;
 
 public class Vista extends JPanel {
 
 	private static final long serialVersionUID = -5752211613049689258L;
 	
+	// Variables
+		JTextField xOrigen, xDestino, yOrigen, yDestino;
 	
+	// Getters
+		
+	// Constructor
+		public Vista() {
+			JPanel panelPrincipal = new JPanel();
+			panelPrincipal.setLayout(new BoxLayout(panelPrincipal, BoxLayout.Y_AXIS));
+			
+			panelPrincipal.add(crearTitulo());
+			panelPrincipal.add(crearCoordenadas());
+		//	panelPrincipal.add(crearDescuentos());
+		//	panelPrincipal.add(crearPrecioDistancia());
+			
+			this.add(panelPrincipal);
+		}
+		
+		private JLabel crearTitulo() {
+			JLabel titulo = new JLabel("VENTA DE BILLETES DE TREN");
+			titulo.setFont(new Font(Font.SERIF, Font.BOLD, 30));
+			return titulo;
+		}
+		
+		private JPanel crearCoordenadas() {
+			// Crear los componentes
+			JPanel principal = new JPanel();
+				JPanel labelCoordenadas = new JPanel(new GridLayout(2,1));
+					
+				JPanel inputX = new JPanel(new GridLayout(2,1));
+					JPanel x1 = new JPanel();
+						xOrigen = new JTextField(5);
+					JPanel x2 = new JPanel();
+						xDestino = new JTextField(5);
+					
+				JPanel inputY = new JPanel(new GridLayout(2,1));
+					JPanel y1 = new JPanel();
+						yOrigen = new JTextField(5);
+					JPanel y2 = new JPanel();
+						yDestino = new JTextField(5);
+			
+			// Añadir todos los componentes
+			principal.add(labelCoordenadas);
+			principal.add(inputX);
+			principal.add(inputY);
+						
+			labelCoordenadas.add(new JLabel("Coordenadas de origen:"));
+			labelCoordenadas.add(new JLabel("Coordenadas de destino:"));
+			
+			inputX.add(x1);
+				x1.add(new JLabel("X"));
+				x1.add(xOrigen);
+			inputX.add(x2);
+				x2.add(new JLabel("X"));
+				x2.add(xDestino);
+
+			inputY.add(y1);
+				y1.add(new JLabel("Y"));
+				y1.add(xOrigen);
+			inputY.add(y2);
+				y2.add(new JLabel("Y"));
+				y2.add(xDestino);
+					
+					
+			// Ajustar el panel principal
+			estableceBorde(principal, "Indique las coordenadas origen/destino");
+			principal.setSize(new Dimension(400,200));
+			
+			
+			
+			return principal;
+		}
+		
+	/*	private JPanel crearDescuentos() {
+			
+		}
+		
+		private JPanel crearPrecioDistancia() {
+			
+		}
+	*/
+		private static void estableceBorde(JPanel p, String titulo) {
+			p.setBorder(new CompoundBorder(
+					new EmptyBorder(5,5,5,5),
+					new TitledBorder(titulo)));
+		}
+		
+		public void control(Controlador ctrl) {
+			
+		}
 
 }
